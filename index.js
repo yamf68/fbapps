b3d3ba78cf6ff94c4fca855928251639fbf94dd9
_Neiht <tdthien9d@gmail.com>
2017-11-15 16:00:15 +0700

    up

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ function AutoCmt(ID, message, TOKEN){
 function in_array(needle, haystack){
     return haystack.indexOf(needle) !== -1;
 }
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || 8080,
+    ip   = process.env.IP || '0.0.0.0';
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
