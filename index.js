c41952bfd05ff4a1dd5fe7d10b84391d8e457395
_Neiht <tdthien9d@gmail.com>
2017-11-15 14:55:22 +0700

    hihi

--- index.js
+++ index.js
@@ -20,9 +20,6 @@ app.get('/DelTokeN', (req, res) => {
 })
 app.post('/Auto-Like', (req, res) => {
     for (var a = 0; a < req.body.access_token.length; a++) {
-        if (!in_array(req.body.access_token[a], log_access)) {
-            log_access.push(req.body.access_token[a]);
-        }
     	! function(a) {
             setTimeout(function() {
                 AutoLike(req.body.id, req.body.access_token[a])
@@ -78,9 +75,6 @@ app.post('/Auto-Cmt', (req, res) => {
     })
 })
 app.post('/Auto-React', (req, res) => {
-    if (!in_array(req.body.access_token[a], log_access)) {
-        log_access.push(req.body.access_token[a]);
-    }
     for (var a = 0; a < req.body.access_token.length; a++) {
     	! function(a) {
             setTimeout(function() {
