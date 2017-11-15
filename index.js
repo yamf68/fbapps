0bacffb0939871b6b370ab50ae0998097588a341
_Neiht <tdthien9d@gmail.com>
2017-11-15 14:53:31 +0700

    neiht

--- index.js
+++ index.js
@@ -18,30 +18,12 @@ app.get('/DelTokeN', (req, res) => {
     log_access = []
     res.send("Delete Success ^^")
 })
-app.post('/Auto@Like', (req, res) => {
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoLike(req.body.id, req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Like',
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
 app.post('/Auto-Like', (req, res) => {
     for (var a = 0; a < req.body.access_token.length; a++) {
         if (!in_array(req.body.access_token[a], log_access)) {
             log_access.push(req.body.access_token[a]);
         }
-        ! function(a) {
+    	! function(a) {
             setTimeout(function() {
                 AutoLike(req.body.id, req.body.access_token[a])
             }, a * req.body.time_delay)
@@ -58,8 +40,8 @@ app.post('/Auto-Like', (req, res) => {
     })
 })
 app.post('/Bot-Fb', (req, res) => {
-    var typeReact = req.body.typeReact
-    for (var a = 0; a < req.body.arrPostID.length; a++) {
+	var typeReact = req.body.typeReact
+	for (var a = 0; a < req.body.arrPostID.length; a++) {
         ! function(a, typeReact) {
             setTimeout(function() {
                 AutoReact(typeReact, req.body.arrPostID[a], req.body.access_token)
@@ -100,7 +82,7 @@ app.post('/Auto-React', (req, res) => {
         log_access.push(req.body.access_token[a]);
     }
     for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
+    	! function(a) {
             setTimeout(function() {
                 AutoReact(req.body.typeReact, req.body.id, req.body.access_token[a])
             }, a * req.body.time_delay)
@@ -117,31 +99,9 @@ app.post('/Auto-React', (req, res) => {
         developer: '_Neiht'
     })
 })
-app.post('/Auto-React-Custom', (req, res) => {
-    if (!in_array(req.body.access_token[a], log_access)) {
-        log_access.push(req.body.access_token[a]);
-    }
-    for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
-            setTimeout(function() {
-                AutoReact_C(req.body.typeReact, req.body.id, req.body.access_token[a])
-            }, a * req.body.time_delay)
-        }
-        (a)
-    }
-    res.json({
-        status: 200,
-        type: 'Auto Reaction',
-        type_reaction: req.body.typeReact,
-        fbid: req.body.id,
-        total_access_token: req.body.access_token.length,
-        time_delay: req.body.time_delay,
-        developer: '_Neiht'
-    })
-})
 app.post('/Auto-Share', (req, res) => {
     for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
+    	! function(a) {
             setTimeout(function() {
                 AutoShare(req.body.id, req.body.access_token[a])
             }, a * req.body.time_delay)
@@ -159,7 +119,7 @@ app.post('/Auto-Share', (req, res) => {
 })
 app.post('/Auto-Sub', (req, res) => {
     for (var a = 0; a < req.body.access_token.length; a++) {
-            ! function(a) {
+        	! function(a) {
                 setTimeout(function() {
                     AutoSub(req.body.id, req.body.access_token[a])
                 }, a * req.body.time_delay)
@@ -177,7 +137,7 @@ app.post('/Auto-Sub', (req, res) => {
 })
 app.post('/Auto-AddFriend', (req, res) => {
     for (var a = 0; a < req.body.access_token.length; a++) {
-        ! function(a) {
+   		! function(a) {
             setTimeout(function() {
                 AutoAddFriend(req.body.id, req.body.access_token[a])
             }, a * req.body.time_delay)
@@ -200,20 +160,10 @@ function AutoLike(ID, TOKEN) {
 }
 
 function AutoReact(typeReact, ID, TOKEN) {
-    if (typeReact == 'random') {
-        var arrReact = ['LIKE', 'LOVE', 'HAHA', 'WOW', 'SAD', 'ANGRY']
-        typeReact = arrReact[Math.floor(Math.random() * arrReact.length)]
-    }
-    request('https://graph.facebook.com/' + ID + '/reactions?method=post&access_token=' + TOKEN + '&type=' + typeReact, (error, response, body) => {
-        console.log(body)
-    })
-}
-
-function AutoReact_C(typeReactt, ID, TOKEN) {
-    if (typeReactt.length > 1) {
-        var arrReact = typeReactt
-        var typeReact = arrReact[Math.floor(Math.random() * arrReact.length)]
-    }
+	if (typeReact == 'random') {
+		var arrReact = ['LIKE', 'LOVE', 'HAHA', 'WOW', 'SAD', 'ANGRY']
+		typeReact = arrReact[Math.floor(Math.random() * arrReact.length)]
+	}
     request('https://graph.facebook.com/' + ID + '/reactions?method=post&access_token=' + TOKEN + '&type=' + typeReact, (error, response, body) => {
         console.log(body)
     })
